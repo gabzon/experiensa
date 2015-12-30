2e1eaedcbbba449a2565ce38c6a266c25a27f2f6
gabzon <gab.zambrano@gmail.com>
2015-12-30 15:18:23 +0100

    improve refundable and flexible display

--- templates/voyage/conditions.php
+++ templates/voyage/conditions.php
@@ -12,45 +12,48 @@ $voyage_info = get_post_meta(get_the_ID(), 'voyage_information_conditions', true
         <div class="ui two column grid stackable">
             <?php foreach ($voyage_conditions as $key => $value): ?>
                 <div class="column">
-                    <div class="ui green message">
-                        <?php if ($value === 'refundable'): ?>
+                    <?php if ($value === 'refundable'): ?>
+                        <div class="ui green message">
                             <?php _e('* This offer is refundable, please look our refund conditions') ?>
-                        <?php elseif ($value === 'flexible'): ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($value === 'flexible'): ?>
+                        <div class="ui green message">
                             <?php _e('* This offer is flexible, please look our flexibility conditions') ?>
-                        <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+        <h3><?php _e('Information & Conditions','sage'); ?></h3>
+        <?= $voyage_info; ?>
+        <br><br>
+    <?php endif; ?>
+
+    <?php if ($included || $excluded): ?>
+        <hr>
+        <br>
+        <div class="ui two column grid stackable">
+            <div class="column">
+                <h3><?php _e('Included','sage') ?></h3>
+                <?php if ($included): ?>
+                    <ul>
+                        <?php foreach ($included as $key): ?>
+                            <li><strong><?= $key->name; ?></strong> <?= $key->description; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+            <div class="column">
+                <h3><?php _e('Not included','sage') ?></h3>
+                <?php if ($excluded): ?>
+                    <ul>
+                        <?php foreach ($excluded as $key): ?>
+                            <li><strong><?= $key->name; ?></strong> <?= $key->description; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
         </div>
     <?php endif; ?>
-    <h3><?php _e('Information & Conditions','sage'); ?></h3>
-    <?= $voyage_info; ?>
-    <br><br>
-<?php endif; ?>
-
-<?php if ($included || $excluded): ?>
-    <hr>
-    <br>
-    <div class="ui two column grid stackable">
-        <div class="column">
-            <h3><?php _e('Included','sage') ?></h3>
-            <?php if ($included): ?>
-                <ul>
-                    <?php foreach ($included as $key): ?>
-                        <li><strong><?= $key->name; ?></strong> <?= $key->description; ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </div>
-        <div class="column">
-            <h3><?php _e('Not included','sage') ?></h3>
-            <?php if ($excluded): ?>
-                <ul>
-                    <?php foreach ($excluded as $key): ?>
-                        <li><strong><?= $key->name; ?></strong> <?= $key->description; ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </div>
-    </div>
-<?php endif; ?>
