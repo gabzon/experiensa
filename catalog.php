c283c7ad0aac049da87974c51c1b63af4b1408b1
gabzon <gab.zambrano@gmail.com>
2015-12-31 15:41:20 +0100

    price and image cover added to api

--- catalog.php
+++ catalog.php
@@ -1,14 +1,53 @@
 <?php
 /**
- * Template Name: Catalog
- */
+* Template Name: Catalog
+*/
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<?php $response = file_get_contents('http://fiestatravel.ch/wp-json/posts?type=product'); ?>
-<?php $response = json_decode($response); ?>
-<?php Piklist::pre($response) ?>
+<?php $fiesta = file_get_contents('http://fiestatravel.ch/wp-json/wp/v2/posts?type=product'); ?>
+<?php $colombia = file_get_contents('http://discovering.experiensa.com/wp-json/wp/v2/voyage'); ?>
+<?php $own = file_get_contents('http://localhost/fiesta-travel/wp-json/wp/v2/voyage'); ?>
+<?php $fiesta = json_decode($fiesta); ?>
+<?php $colombia = json_decode($colombia); ?>
+<?php $own = json_decode($own); ?>
+<?php $voyages = array(); ?>
+<div class="ui container">
+    <?php foreach ($fiesta as $key => $value): ?>
+        <?php array_push($voyages, $value); ?>
+    <?php endforeach; ?>
+    <?php foreach ($colombia as $key => $value): ?>
+        <?php array_push($voyages, $value); ?>
+    <?php endforeach; ?>
+    <?php foreach ($own as $key => $value): ?>
+        <?php array_push($voyages, $value); ?>
+    <?php endforeach; ?>
+    <br>
+    <div class="ui three column grid stackable">
+        <?php foreach ($voyages as $key => $value): ?>
+            <div class="column">
+                <div class="ui card">
+                    <div class="image">
+                        <?php $img =  $value->featured_image; ?>
+                        <?php $feature_img =  wp_get_attachment_image_src( $img ); ?>
+                        <img src="<?= $feature_img[0]  ?>" alt="" />
+                    </div>
+                    <div class="content">
+                        <div class="header">
+                            <?php echo $value->title->rendered; ?>
+                        </div>
+                        <div class="meta">
+                            <?php echo $value->excerpt->rendered; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <?php Piklist::pre($voyages); ?>
+</div>
--- modules/voyages/api-voyages.php
+++ modules/voyages/api-voyages.php
@@ -1,30 +1,43 @@
 <?php
 
-/**
-* Grab latest post title by an author!
-*
-* @param array $data Options for the function.
-* @return string|null Post title for the latest,
-* or null if none.
-*/
-
-function my_awesome_func( $data ) {
-    $posts = get_posts( array(
-        'author' => $data['id'],
-    ) );
-
-    if ( empty( $posts ) ) {
-        return null;
-    }
-
-    return $posts[0]->post_title;
+add_action( 'rest_api_init', 'slug_register_voyage_price' );
+function slug_register_voyage_price() {
+    register_rest_field( 'voyage', 'voyage_price',
+        array(
+            'get_callback'    => 'slug_get_voyage_price',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
 }
 
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'myplugin/v1', '/author/(?P<id>\d+)', array(
-        'methods' => 'GET',
-        'callback' => 'my_awesome_func',
-    ) );
-} );
+add_action( 'rest_api_init', 'slug_register_voyage_cover_image' );
+function slug_register_voyage_cover_image() {
+    register_rest_field( 'voyage', 'cover_image',
+        array(
+            'get_callback'    => 'slug_get_voyage_cover_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+/**
+ * Get the value of the "starship" field
+ *
+ * @param array $object Details of current post.
+ * @param string $field_name Name of field.
+ * @param WP_REST_Request $request Current request
+ *
+ * @return mixed
+ */
+function slug_get_voyage_price( $object, $field_name, $request ) {
+    return get_post_meta( $object[ 'id' ], $field_name, true );
+}
+
+function slug_get_voyage_cover_image( $object, $field_name, $request ) {
+    $cover_image = wp_get_attachment_image_src( get_post_thumbnail_id($object['id']), 'full' );
+    return $cover_image[0];
+}
 
 ?>
