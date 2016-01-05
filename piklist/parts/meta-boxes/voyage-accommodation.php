67447aeee897cd720795b9188a9b9b70d1f1d036
gabzon <gab.zambrano@gmail.com>
2016-01-05 22:16:20 +0100

    french and spanish languages

--- piklist/parts/meta-boxes/voyage-accommodation.php
+++ piklist/parts/meta-boxes/voyage-accommodation.php
@@ -12,15 +12,15 @@ Flow: Voyage Workflow
 
 $voyage_host_name = array(
     'type'      => 'text',
-    'field'     => 'voyage_etablishment_name',
-    'label'     => __('Etablishment name','sage'),
+    'field'     => 'voyage_establishment_name',
+    'label'     => __('Establishment name','sage'),
     'columns'   => 6
 );
 
 $voyage_host_type = array(
     'type'      => 'select',
-    'field'     => 'voyage_etablishment_type',
-    'label'     => __('Etablishment type','sage'),
+    'field'     => 'voyage_establishment_type',
+    'label'     => __('Establishment type','sage'),
     'columns'   => 4,
     'choices'   => array(
         'hotel'     => __('Hotel','sage'),
@@ -34,7 +34,7 @@ $voyage_host_type = array(
 
 $voyage_host_rating = array(
     'type'      => 'select',
-    'field'     => 'voyage_etablishment_rating',
+    'field'     => 'voyage_establishment_rating',
     'label'     => __('Rating','sage'),
     'columns'   => 2,
     'choices'   => array(
@@ -48,7 +48,7 @@ $voyage_host_rating = array(
 
 $voyage_host_checkin_date = array(
     'type'      => 'datepicker',
-    'field'     => 'voyage_etablishment_checkin_date',
+    'field'     => 'voyage_establishment_checkin_date',
     'label'     => __('Check-in date','sage'),
     'columns'   => 3,
     'options'   => array('dateFormat' => 'd/m/yy'),
@@ -56,14 +56,14 @@ $voyage_host_checkin_date = array(
 
 $voyage_host_checkin_time = array(
     'type'      => 'text',
-    'field'     => 'voyage_etablishment_checkin_time',
+    'field'     => 'voyage_establishment_checkin_time',
     'label'     => __('Check-in time','sage'),
     'columns'   => 3
 );
 
 $voyage_host_checkout_date = array(
     'type'      => 'datepicker',
-    'field'     => 'voyage_etablishment_checkout_date',
+    'field'     => 'voyage_establishment_checkout_date',
     'label'     => __('Check-out date','sage'),
     'columns'   => 3,
     'options'   => array('dateFormat' => 'd/m/yy'),
@@ -71,21 +71,21 @@ $voyage_host_checkout_date = array(
 
 $voyage_host_checkout_time = array(
     'type'      => 'text',
-    'field'     => 'voyage_etablishment_checkout_time',
+    'field'     => 'voyage_establishment_checkout_time',
     'label'     => __('Check-out time','sage'),
     'columns'   => 3
 );
 
 $voyage_host_comments = array(
     'type'      => 'textarea',
-    'field'     => 'voyage_etablishment_comments',
+    'field'     => 'voyage_establishment_comments',
     'label'     => __('Comments & Description','sage'),
     'columns'   => 12
 );
 
 $voyage_host_gallery = array(
     'type'      => 'file',
-    'field'     => 'voyage_etablishment_gallery',
+    'field'     => 'voyage_establishment_gallery',
     'label'     => __('Photo Gallery','sage'),
 );
 
