a98c9b933fdff297e25f682e00407dcad89eb2a0
Victor Valencia <valencia6xgmail.com>
2016-03-16 11:26:56 -0430

    timezone data on agency information

--- piklist/parts/settings/agency-information.php
+++ piklist/parts/settings/agency-information.php
@@ -3,6 +3,7 @@
  Title: Agency Information
  Setting: agency_settings
  */
+//use Roots\Sage\TZ\Timezone;
 
 piklist('field', array(
     'type'      => 'file',
@@ -70,6 +71,53 @@ piklist('field', array(
         'class' => 'regular-text',
         'placeholder' => __('Enter the agency country','sage')
     )
+));
+
+
+function get_timezone_array(){
+    $timezoneIdentifiers = DateTimeZone::listIdentifiers();
+    $utcTime = new DateTime('now', new DateTimeZone('UTC'));
+
+    $tempTimezones = array();
+    foreach ($timezoneIdentifiers as $timezoneIdentifier) {
+        $currentTimezone = new DateTimeZone($timezoneIdentifier);
+
+        $tempTimezones[] = array(
+            'offset' => (int)$currentTimezone->getOffset($utcTime),
+            'identifier' => $timezoneIdentifier
+        );
+    }
+
+    // Sort the array by offset,identifier ascending
+    usort($tempTimezones, function($a, $b) {
+        return ($a['offset'] == $b['offset'])
+            ? strcmp($a['identifier'], $b['identifier'])
+            : $a['offset'] - $b['offset'];
+    });
+
+    $timezoneList = array();
+    foreach ($tempTimezones as $tz) {
+        $sign = ($tz['offset'] > 0) ? '+' : '-';
+        $offset = gmdate('H:i', abs($tz['offset']));
+        $timezoneList[$tz['identifier']] = '(UTC ' . $sign . $offset . ') ' .
+            $tz['identifier'];
+    }
+
+    return $timezoneList;
+}
+$vector = get_timezone_array();
+
+/*$tz = new Timezone();
+$vector = $tz->get_timezone_array();*/
+piklist('field', array(
+    'type' => 'select',
+    'field' => 'agency_timezone',
+    'scope' => 'post_meta',
+    'label' => __('Timezone'),
+    'attributes' => array(
+        'class' => 'text'
+    ),
+    'choices' => $vector
 ));
 
 piklist('field', array(
