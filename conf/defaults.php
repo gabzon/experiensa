23faa80f76131724e6a6e2714ed02364effe63a3
Victor Valencia <valencia6x@gmail.com>
2016-04-28 12:27:29 -0430

    post-types validation

--- conf/defaults.php
+++ conf/defaults.php
@@ -16,28 +16,31 @@ add_filter('upload_mimes', 'cc_mime_types');
 */
 function hide_custom_post_types(){
     $agency_options = get_option('agency_settings');
-    $website_use = $agency_options['website_use'];
-    $all_posttypes = ["brochure","partner","feedback","service","host","voyage","estimate"];
-    if(isset($website_use)){
-        $posttypes = array();
-        switch($website_use){
-            case 'travel':
-                $posttypes = $agency_options['travel_agency_posttypes'];
-                break;
-            case 'hotel':
-                $posttypes = $agency_options['hotel_posttypes'];
-                break;
-            default:
-                $posttypes = $agency_options['tourist_office_posttypes'];
-                break;
+    if(isset($agency_options)) {
+        $website_use = $agency_options['website_use'];
+        $all_posttypes = ["brochure", "partner", "feedback", "service", "host", "voyage", "estimate"];
+        $to_hide = array();
+        if (isset($website_use)) {
+            $posttypes = array();
+            switch ($website_use) {
+                case 'travel':
+                    $posttypes = $agency_options['travel_agency_posttypes'];
+                    break;
+                case 'hotel':
+                    $posttypes = $agency_options['hotel_posttypes'];
+                    break;
+                default:
+                    $posttypes = $agency_options['tourist_office_posttypes'];
+                    break;
+            }
+            if (count($posttypes) == 1)
+                $posttypes = array($posttypes);
+            $to_hide = array_diff($all_posttypes, $posttypes);
         }
-        if(count($posttypes)==1)
-            $posttypes= array($posttypes);
-        $to_hide = array_diff($all_posttypes,$posttypes);
-    }
-    foreach($to_hide as $value){
-        if(post_type_exists($value))
-            remove_menu_page( 'edit.php?post_type='.$value );
+        foreach ($to_hide as $value) {
+            if (post_type_exists($value))
+                remove_menu_page('edit.php?post_type=' . $value);
+        }
     }
 }
 
