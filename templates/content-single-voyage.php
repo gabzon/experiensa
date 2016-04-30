a22847ab0d120d969da05b59e8dfc3d064234dbe
gabzon <gab.zambrano@gmail.com>
2016-04-30 12:12:38 +0200

    change voyage_expery_date to expiry_date

--- templates/content-single-voyage.php
+++ templates/content-single-voyage.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 $agency_options = get_option('agency_settings');
 $timezone = $agency_options['agency_timezone'];
-date_default_timezone_set($timezone); 
+date_default_timezone_set($timezone);
 ?>
 <?php while (have_posts()) : the_post(); ?>
-    <?php $voyage_expiry_date = get_post_meta($post->ID, 'voyage_expiry_date', true); /*$pepe = $post->ID;*/?>
+    <?php $voyage_expiry_date = get_post_meta($post->ID, 'expiry_date', true); ?>
     <?php $voyage_expiry_date_formatted = DateTime::createFromFormat('d/m/Y', $voyage_expiry_date)->format('Y-m-d'); ?>
     <?php $post_status = get_post_status( $post->ID );?>
     <?php $countries = get_the_terms($post->ID ,'country');?>
@@ -31,12 +31,7 @@ date_default_timezone_set($timezone);
             </script>
         <?php else:
             $postid = Common::get_original_post_id($post->ID,'voyage');
-            /*echo "traducido: ".$postid;
-            echo " original: ".$post->ID;*/
             $terms = Common::get_media_terms($postid,'voyage');
-            /*print_r($terms);
-            $ttt = Common::get_media_terms($postid,'voyage');
-            print_r($ttt);*/
             $gallery = RequestMedia::get_media_request_api('media',$terms);
         ?>
           <script type="text/javascript">
@@ -81,7 +76,7 @@ date_default_timezone_set($timezone);
             <div class="ui red message">
                 <?php _e('Sorry for the inconvenience. This offer is not longer available','sage'); ?>
                 <?php if($post_status=='publish'):?>
-                    <?php 
+                    <?php
                     $my_post = array();
                     $my_post['ID'] = $post->ID;
                     $my_post['post_status'] = 'draft';
@@ -97,40 +92,3 @@ date_default_timezone_set($timezone);
     <?php endif; ?>
 
 <?php endwhile; ?>
-<?php
-  /*$mediaapi = RequestMedia::get_media_request('media','location','peru');
-  echo "<pre>";
-  print_r($mediaapi);
-  echo "</pre>";
-  echo "<pre>";
-  print_r($countries);
-  echo "</pre>";
-  echo "<pre>";
-  print_r($locations);
-  echo "</pre>";*/
-/*  echo "<pre>";
-  print_r($gallery);
-  echo "</pre>";
-  print_r($terms);
-    echo"-------------".$postid;
-print_r(Common::get_custom_taxonomies_by_pt('voyage'));
-print_r(Common::get_terms_by_id_taxonomies($postid,['location']));
-print_r(Common::get_terms_by_id_taxonomies($pepe,['location']));
-echo "<br>prueba<br>";
-print_r(icl_object_id($postid, 'location', true, 'en'));
-//echo"aaaaaaaaaaaaaaaaaaaaaaaaaaaaa-".translated_taxonomy_slug('japon','en');
-echo"id del lenguaje actual ".$pepe." <br>";
-echo"id del lenguaje original ".$postid." <br>";
-echo"taxonomias<br>";
-print_r(get_object_taxonomies( 'voyage' ));
-echo"term original<br>";
-print_r(get_the_terms($postid,'location'));
-echo"term traducido<br>";
-print_r(get_the_terms($pepe,'location'));
-echo"terminos actual<br>";
-print_r(wp_get_post_terms($postid,'location'));
-echo"terminos original<br>";
-print_r(wp_get_post_terms($pepe,'location'));
-
-echo"lenguaje acutal ".ICL_LANGUAGE_CODE ;*/
-?>
