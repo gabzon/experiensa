0886e9b075bafd0dc5f1c57d966125900b7c84ad
Victor Valencia <valencia6x@gmail.com>
2016-05-05 20:40:12 -0430

    testing htacssess param

--- templates/landing/partners.php
+++ templates/landing/partners.php
@@ -1,6 +1,6 @@
 <?php
 $agency_settings = get_option('agency_settings');
-$partners = $agency_settings['agency_partners'];
+$partners = (isset($agency_settings['agency_partners'])?$agency_settings['agency_partners']:array());
 
 $design_options = get_option('experiensa_design_settings');
 $section = $design_options['promotion_color_group'];
@@ -14,25 +14,25 @@ $partner_logo = $partners['partner_logo'];
 
 if(!empty($partners)):
 ?>
-  <section id="promotion" class="ui basic <?= get_the_color($color, $inverted[0]); ?> vertical segment center aligned">
-    <br><br>
-    <div class="ui container">
-      <h1><?php _e('Our Partners','sage'); ?></h1><br>
-      <?php
-      for($i=0;$i<count($partner_name);$i++):
-        $partner['title'] = $partner_name[$i];
-        $partner['subtitle'] = '';
-        $partner['post_link'] = (isset($partner_website[$i]) && !empty($partner_website[$i])?$partner_website[$i]:'#');
-        $partner['image_url'] = wp_get_attachment_url($partner_logo[$i][0]);
-        $partner['thumbnail_url'] = (isset($partner_logo[$i][0]) && !empty($partner_logo[$i][0])?wp_get_attachment_thumb_url($partner_logo[$i][0]):'');
-        //$partner['thumbnail_image'] = (isset($partner_logo[$i][0]) && !empty($partner_logo[$i][0])?wp_get_attachment_image($partner_logo[$i][0],'thumbnail'):'');
+    <section id="promotion" class="ui basic <?= get_the_color($color, $inverted[0]); ?> vertical segment center aligned">
+        <br><br>
+        <div class="ui container">
+        <h1><?php _e('Our Partners','sage'); ?></h1><br>
+        <?php
+        for($i=0;$i<count($partner_name);$i++):
+            $partner['title'] = $partner_name[$i];
+            $partner['subtitle'] = '';
+            $partner['post_link'] = (isset($partner_website[$i]) && !empty($partner_website[$i])?$partner_website[$i]:'#');
+            $partner['image_url'] = wp_get_attachment_url($partner_logo[$i][0]);
+            $partner['thumbnail_url'] = (isset($partner_logo[$i][0]) && !empty($partner_logo[$i][0])?wp_get_attachment_thumb_url($partner_logo[$i][0]):'');
+            //$partner['thumbnail_image'] = (isset($partner_logo[$i][0]) && !empty($partner_logo[$i][0])?wp_get_attachment_image($partner_logo[$i][0],'thumbnail'):'');
         
-        $partner_list[] = $partner;
-      endfor;
-      //Carousel::display_carousel($partner_list);
-      Grid::display_grid($partner_list);
+            $partner_list[] = $partner;
+        endfor;
+        //Carousel::display_carousel($partner_list);
+        Grid::display_grid($partner_list);
       ?>
     </div>
-  </section>
+    </section>
 <?php
 endif;
