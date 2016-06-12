30fe541201477287c2616d3dd4b13586e8796ef3
Victor Valencia <valencia6x@gmail.com>
2016-06-11 23:52:01 -0430

    new header layout

--- templates/header.php
+++ templates/header.php
@@ -7,8 +7,7 @@ $design_options = get_option('experiensa_design_settings');
 $header_style = get_theme_mod('header_style');
 $logo   = $agency_options['agency_logo'];
 $phone  = $agency_options['agency_phone'];
-
 if($header_style!=null):
     Header::get_header($header_style,get_the_ID(),true);
 endif;
-?>
+
