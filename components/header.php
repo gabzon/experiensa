9a3e57e1edfc649dcdba0258970fcb2a858843e2
gabzon <gab.zambrano@gmail.com>
2016-06-14 10:16:13 +0200

    header mobile debugged

--- components/header.php
+++ components/header.php
@@ -272,7 +272,7 @@ Class Header{
         $logo = $agency_options['agency_logo'];
         $url = esc_url(home_url('/'));
         $mobile =   "<div class=\"mobile only row\">";
-        $mobile .=      "<div class=\"ui ".get_menu_style()." menu grid\">";
+        $mobile .=      "<div class=\"ui ".get_menu_style()." menu grid header-menu\">";
         $mobile .=          "<a class=\"item\" href=\"".$url."\">";
         if ($logo):
             $mobile .=          '<img class="ui tiny image logo" src="' . wp_get_attachment_url($logo) . '"  />';
