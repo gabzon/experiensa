6e0ecb8d1846179c0ce396da0a342c5ebb26da96
gabzon <gab.zambrano@gmail.com>
2016-06-24 15:13:32 +0200

    small changes

---
+++ dist/scripts/customizer.js
@@ -0,0 +1,2 @@
+!function(e){wp.customize("blogname",function(n){n.bind(function(n){e(".brand").text(n)})})}(jQuery),jQuery(document).ready(function(){jQuery('.customize-control-checkbox-multiple input[type="checkbox"]').on("change",function(){checkbox_values=jQuery(this).parents(".customize-control").find('input[type="checkbox"]:checked').map(function(){return this.value}).get().join(","),jQuery(this).parents(".customize-control").find('input[type="hidden"]').val(checkbox_values).trigger("change")})});
+//# sourceMappingURL=customizer.js.map
