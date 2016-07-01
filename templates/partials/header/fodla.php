93c4b9b65dbf756fc2cc52dc48b2c87fe61c77d6
Victor Valencia <valencia6x@gmail.com>
2016-07-01 08:55:37 -0430

    fodla header update

--- templates/partials/header/fodla.php
+++ templates/partials/header/fodla.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<script type="text/javascript">
-    jQuery(document).ready(function(){
-        jQuery('.right.menu.open').on("click",function(e){
-            e.preventDefault();
-            jQuery('.ui.vertical.menu').toggle();
-        });
-        jQuery('.ui.dropdown').dropdown();
-    });
-</script>
-<!-- Fodla Layout -->
-<div id="header-nav" class="ui grid">
-    <div class="computer tablet only row">
-        <div class="ui <?= Header::get_menu_style(); ?> menu navbar grid borderless header-menu">
-            <?= Header::header_logo_item();?>
-            <?= Header::get_website_name_tagline(); ?>
-            <div class="right menu">
-                <?php
-                echo Header::get_phone_item('item');
-                echo Header::get_quote_item('item');
-                Menu::display_all_menus($page_id,'left',false);
-                echo Header::get_language_item('item');
-                ?>
-            </div>
-        </div>
-    </div>
-    <?php get_template_part('templates/partials/header/mobile'); ?>
-</div>
-=======
 <script type="text/javascript">
     jQuery(document).ready(function(){
         jQuery('.right.menu.open').on("click",function(e){
