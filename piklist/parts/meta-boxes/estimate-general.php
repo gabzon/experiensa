38cb638da1a552ee8dcbd062790732f7d4c83e75
Víctor Valencia <valencia6x@gmail.com>
2016-08-17 19:40:40 -0400

    changes on estimate metaboxes

--- piklist/parts/meta-boxes/estimate-general.php
+++ piklist/parts/meta-boxes/estimate-general.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Title: Voyage
+Title: Estimate Voyages
 Post Type: estimate
 Meta box: true
 order:1
@@ -10,7 +10,7 @@ $title = array(
     'type'          => 'text',
     'field'         => 'estimate_title',
     'label'         => __('Title','sage'),
-    'columns'       => 9
+    'columns'       => 8
 );
 
 $people = array(
@@ -18,24 +18,22 @@ $people = array(
     'field' => 'estimate_people',
     'label' => __('Number of people','sage'),
     'validate'      => [ ['type' => 'number'] ],
-    'columns' => 1,
+    'columns' => 2,
 );
 
 $number_days = array(
-    'type'          => 'text',
+    'type'          => 'number',
     'field'         => 'estimate_days',
     'label'         => __('Days','sage'),
     'columns'       => 1,
-    'attributes'    => array( 'placeholder' => __('Days','sage')),
     'validate'      => [ ['type' => 'number'] ]
 );
 
 $number_nights = array(
-    'type'          => 'text',
+    'type'          => 'number',
     'field'         => 'estimate_nights',
     'label'         => __('Nights','sage'),
     'columns'       => 1,
-    'attributes'    => ['placeholder' => __('Nights')],
     'validate'      => array( array( 'type' => 'number' ) )
 );
 
@@ -83,27 +81,29 @@ $photos = array(
     'label' => __('Photo Gallery','sage')
 );
 
+
+
 /*******************************************************************************
-** Flight Section
-*******************************************************************************/
+ ** Flight Section
+ *******************************************************************************/
 
 $flight_airline = array(
     'type'  => 'text',
-    'field' => 'estimate_flight_airline',
+    'field' => 'flight_airline',
     'columns'   => 4,
     'label' => __('Airline','sage')
 );
 
 $flight_number = array(
     'type'  => 'text',
-    'field' => 'estimate_flight_number',
+    'field' => 'flight_number',
     'columns'   => 4,
     'label' => __('Flight number','sage')
 );
 
 $flight_class = array(
     'type'      => 'select',
-    'field'     => 'estimate_flight_class',
+    'field'     => 'flight_class',
     'label'     => __('Flight Class','sage'),
     'columns'   => 4,
     'value'     => 'economy',
@@ -116,63 +116,62 @@ $flight_class = array(
 
 $flight_departure_city = array(
     'type'      => 'text',
-    'field'     => 'estimate_flight_departure_city',
+    'field'     => 'flight_departure_city',
     'columns'   => 6,
     'label'     => __('Departure city','sage')
 );
 
 $flight_arrival_city = array(
     'type'      => 'text',
-    'field'     => 'estimate_flight_arrival_city',
+    'field'     => 'flight_arrival_city',
     'columns'   => 6,
     'label'     => __('Arrival city','sage')
 );
 
 $flight_departure_date = array(
     'type'      => 'datepicker',
-    'field'     => 'estimate_flight_departure_date',
+    'field'     => 'flight_departure_date',
     'columns'   => 3,
     'label'     => __('Departure date','sage'),
-    'options'   => array( 'dateFormat' => 'd/m/yy' ),
+    'options'   => array( 'dateFormat' => 'dd/mm/yy' ),
 );
 
 $flight_departure_time= array(
-    'type'  => 'text',
-    'field' => 'estimate_flight_departure_time',
+    'type'      => 'time',
+    'field'     => 'flight_departure_time',
     'columns'   => 3,
-    'label' => __('Departure time','sage')
+    'label'     => __('Departure time','sage')
 );
 
 $flight_arrival_date = array(
     'type'      => 'datepicker',
-    'field'     => 'estimate_flight_arrival_date',
+    'field'     => 'flight_arrival_date',
     'columns'   => 3,
     'label'     => __('Arrival date','sage'),
-    'options'   => array('dateFormat' => 'd/m/yy'),
+    'options'   => array('dateFormat' => 'dd/mm/yy'),
 );
 
 $flight_arrival_time = array(
-    'type'      => 'text',
-    'field'     => 'estimate_flight_arrival_time',
+    'type'      => 'time',
+    'field'     => 'flight_arrival_time',
     'columns'   => 3,
     'label'     => __('Arrival time','sage')
 );
 
 $comments = [
     'type'          => 'text',
-    'field'         => 'estimate_flight_comments',
+    'field'         => 'flight_comments',
     'label'         => __('Comments','sage'),
     'attributes'    => ['placeholder' => __('Comments','sage')],
     'columns'       => 12
 ];
 
-$flight = array(
-    'type'      => 'group',
-    'template'  => 'field',
-    'label'     => __('Flight Options','sage'),
-    'field'     => 'estimate_flight_group',
-    'add_more'  => true,
-    'fields'    => array(
+$flights = array(
+    'type' => 'group',
+    'field' => 'flights _group',
+    'label' => __('Flights','sage'),
+    'add_more' => true,
+    'fields' => array(
         $flight_airline,
         $flight_number,
         $flight_class,
@@ -185,21 +184,19 @@ $flight = array(
         $comments
     )
 );
-
 /*******************************************************************************
-** Accommodation Section
-*******************************************************************************/
-
-$estimate_host_name = [
+ ** Accommodation Section
+ *******************************************************************************/
+$host_name = [
     'type'      => 'text',
-    'field'     => 'estimate_establishment_name',
+    'field'     => 'establishment_name',
     'label'     => __('Establishment name','sage'),
     'columns'   => 6
 ];
 
-$estimate_host_type = [
+$host_type = [
     'type'      => 'select',
-    'field'     => 'estimate_establishment_type',
+    'field'     => 'establishment_type',
     'label'     => __('Establishment type','sage'),
     'columns'   => 4,
     'choices'   => array(
@@ -212,9 +209,9 @@ $estimate_host_type = [
     )
 ];
 
-$estimate_host_rating = array(
+$host_rating = array(
     'type'      => 'select',
-    'field'     => 'estimate_establishment_rating',
+    'field'     => 'establishment_rating',
     'label'     => __('Rating','sage'),
     'columns'   => 2,
     'choices'   => array(
@@ -226,80 +223,72 @@ $estimate_host_rating = array(
     )
 );
 
-$estimate_host_checkin_date = array(
+$host_checkin_date = array(
     'type'      => 'datepicker',
-    'field'     => 'estimate_establishment_checkin_date',
+    'field'     => 'establishment_checkin_date',
     'label'     => __('Check-in date','sage'),
     'columns'   => 3,
     'options'   => array('dateFormat' => 'd/m/yy'),
 );
 
-$estimate_host_checkin_time = array(
-    'type'      => 'text',
-    'field'     => 'estimate_establishment_checkin_time',
+$host_checkin_time = array(
+    'type'      => 'time',
+    'field'     => 'establishment_checkin_time',
     'label'     => __('Check-in time','sage'),
     'columns'   => 3
 );
 
-$estimate_host_checkout_date = array(
+$host_checkout_date = array(
     'type'      => 'datepicker',
-    'field'     => 'estimate_establishment_checkout_date',
+    'field'     => 'establishment_checkout_date',
     'label'     => __('Check-out date','sage'),
     'columns'   => 3,
     'options'   => array('dateFormat' => 'd/m/yy'),
 );
 
-$estimate_host_checkout_time = array(
-    'type'      => 'text',
-    'field'     => 'estimate_establishment_checkout_time',
+$host_checkout_time = array(
+    'type'      => 'time',
+    'field'     => 'establishment_checkout_time',
     'label'     => __('Check-out time','sage'),
     'columns'   => 3
 );
+$host_gallery = [
+    'type'      => 'file',
+    'field'     => 'establishment_gallery',
+    'label'     => __('Photo Gallery','sage'),
+];
 
-$estimate_host_comments = array(
+$host_comments = array(
     'type'      => 'textarea',
-    'field'     => 'estimate_establishment_comments',
+    'field'     => 'establishment_comments',
     'label'     => __('Comments & Description','sage'),
     'columns'   => 12
 );
 
-$estimate_host_gallery = [
-    'type'      => 'file',
-    'field'     => 'estimate_establishment_gallery',
-    'label'     => __('Photo Gallery','sage'),
-];
 
-$comments = [
-    'type'      => 'text',
-    'field'     => 'estimate_accommodation_comments',
-    'label'     => __('Comments','sage'),
-    'columns'   => 12
-];
-
-$accommodation_group = array(
-    'type'      => 'group',
-    'template'  => 'field',
-    'field'     => 'estimate_accommodation_group',
-    'add_more'  => true,
-    'fields'    => array(
-        $estimate_host_name,
-        $estimate_host_type,
-        $estimate_host_rating,
-        $estimate_host_checkin_date,
-        $estimate_host_checkin_time,
-        $estimate_host_checkout_date,
-        $estimate_host_checkout_time,
-        $estimate_host_comments,
-        $estimate_host_gallery,
-        $comments,
+$accomodations = array(
+    'type' => 'group',
+    'field' => 'accomodations _group',
+    'label' => __('Accomodations','sage'),
+    'add_more' => true,
+    'fields' => array(
+        $host_name,
+        $host_type,
+        $host_rating,
+        $host_checkin_date,
+        $host_checkin_time,
+        $host_checkout_date,
+        $host_checkout_time,
+        $host_comments,
+        $host_comments
     )
 );
 
 piklist('field', array(
     'type'      => 'group',
+    'field'     => 'estimate_voyages',
     'template'  => 'field',
-    'label'     => __('Options','sage'),
-    'field'     => 'estimate_general_group',
+    'label'     => __('Voyages','sage'),
     'add_more'  => true,
     'fields'    => array(
         $title,
@@ -312,7 +301,9 @@ piklist('field', array(
         $slogan,
         $conditions,
         $photos,
-        $flight,
-        $accommodation_group
+        $flights,
+        $accomodations
+//        $flight,
+//        $accommodation_group
     )
 ));
