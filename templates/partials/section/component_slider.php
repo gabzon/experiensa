33a66557fc3c69b49432160e1cde7eeb07163abd
Victor Valencia <valencia6x@gmail.com>
2016-09-21 09:29:57 -0400

    section title extra space

--- templates/partials/section/component_slider.php
+++ templates/partials/section/component_slider.php
@@ -4,6 +4,8 @@ if($show_layout):?>
 <section id="<?= $name;?>" class="ui <?= $background['class'];?> vertical segment" style="<?= $background['style'];?>">
     <div class="ui <?= $layout['container_class'];?> vertical segment" style="<?= $layout['container_style'].$layout['content_color'];?>">
         <div class="ui <?= $layout['title_alignment']?> header" style="<?= $layout['title_color'];?>">
+            <br>
+            <br>
             <div class="page-header">
                 <h1><?= $layout['title'];?></h1>
                 <?= (!empty($layout['subtitle'])?"<h3>".$layout['subtitle']."</h3><br>":"");?>
--- templates/partials/section/single_page.php
+++ templates/partials/section/single_page.php
@@ -2,6 +2,8 @@
     <div class="ui <?= $layout['container_class'];?> vertical segment" style="<?= $layout['container_style'].$layout['content_color'];?>">
         <br>
         <div class="ui <?= $layout['title_alignment']?> header" style="<?= $layout['title_color'];?>">
+            <br>
+            <br>
             <div class="page-header">
                 <h1><?= $page['title'];?></h1>
             </div>
