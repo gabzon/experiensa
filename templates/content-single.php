49b4e1a71916936154ac3c105a0487eb9510faf4
Víctor Valencia <valencia6x@gmail.com>
2016-10-27 15:23:54 -0400

    validate comments on post content

--- templates/content-single.php
+++ templates/content-single.php
@@ -18,7 +18,11 @@
             <footer>
                 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
             </footer>
-            <?php comments_template('/templates/comments.php'); ?>
+            <?php
+            if(comments_open()):
+                comments_template('/templates/comments.php');
+            endif;
+            ?>
         </article>
     <?php endwhile; ?>
 </div>
