23d62a7ee776bf5ecde68c656847b7ade9ca824e
Víctor Valencia <valencia6x@gmail.com>
2016-11-16 14:12:49 -0400

    added link to grid

--- templates/partials/textimage/overlay.php
+++ templates/partials/textimage/overlay.php
@@ -1,13 +1,15 @@
-<div class="ui image">
-    <div class="<?= $textimage->getVerticalAlignment();?> content" style="<?= $textimage->getHorizontalAlignment();?> position: absolute; height: 100%; width: 100%;z-index: 1;<?= $textimage->getVerticalSimpleAlignment();?>">
-        <div class="center">
-            <div class="header" style="<?= $textimage->getTextStyle();?> font-weight: bold;"><?= $textimage->getTitle();?></div>
-            <br>
-            <div class="description"><?= $textimage->getSubtitle();?></div>
+<a href="<?=$textimage->getPostLink();?>">
+    <div class="ui image">
+        <div class="<?= $textimage->getVerticalAlignment();?> content" style="<?= $textimage->getHorizontalAlignment();?> position: absolute; height: 100%; width: 100%;z-index: 1;<?= $textimage->getVerticalSimpleAlignment();?>">
+            <div class="center">
+                <div class="header" style="<?= $textimage->getTextStyle();?> font-weight: bold;"><?= $textimage->getTitle();?></div>
+                <br>
+                <div class="description"><?= $textimage->getSubtitle();?></div>
+            </div>
         </div>
-    </div>
-    <div class="ui dimmer" style="padding: 5px 5px 5px 5px;">
+        <div class="ui dimmer" style="padding: 5px 5px 5px 5px;">
 
+        </div>
+        <img src="<?=$textimage->getImage();?>" width="100%">
     </div>
-    <img src="<?=$textimage->getImage();?>" width="100%">
-</div>
+</a>
