cec13ec845f9607042485a96b7fce8ed2dde7db3
Víctor Valencia <valencia6x@gmail.com>
2016-11-29 08:14:36 -0400

    Location filter working

--- react-src/actions/index.js
+++ react-src/actions/index.js
@@ -64,8 +64,9 @@ function searchOnCatalog(catalog,themes,locations){
         let nAuxList = auxList.filter(function(elem,pos){
             return auxList.indexOf(elem) == pos
         })
-        if(nAuxList.length > 0)
+        if(nAuxList.length > 0) {
             return nAuxList
+        }
         return []
     }
     return auxList
@@ -115,34 +116,39 @@ export function filterThemeCatalog(filter,add){
             type: FILTER_CATALOG,
             payload: newCatalog,
             originalCatalog: original_state.originalCatalog,
-            // catalogFiltered: newCatalog,
             themes: original_state.theme_filters,
             themes_actives: themes_actives,
             locations: original_state.location_filters,
-            locations_active: original_state.location_filters_active
+            locations_active: location_actives
         })
     }
 }
 
 export function filterLocationCatalog(filter,add){
     return(dispatch,getState)=>{
-        let original_state = getState().catalog;
-        //console.log(original_state)
-        //console.log("el valor de add es "+add)
-        let new_locations_actives
+        let original_state = getState().catalog
+        let originalCatalog = original_state.originalCatalog
+        let themes_actives = original_state.theme_filters_active
+        let location_actives
         if(add){
-            new_locations_actives = add_filter(filter,original_state.location_filters_active)
+            location_actives = add_filter(filter,original_state.location_filters_active)
         }else{
-            new_locations_actives = delete_filter(filter,original_state.location_filters_active)
+            location_actives = delete_filter(filter,original_state.location_filters_active)
         }
-        let newCatalog = filterCatalogByItem(filter,'location',original_state.catalog)
+        let newCatalog
+        if(themes_actives.length < 1 && location_actives.length < 1){
+            newCatalog =  original_state.originalCatalog
+        }else{
+            newCatalog = searchOnCatalog(originalCatalog, themes_actives,location_actives)
+        }
         dispatch({
             type: FILTER_CATALOG,
             payload: newCatalog,
+            originalCatalog: original_state.originalCatalog,
             themes: original_state.theme_filters,
-            themes_actives: original_state.theme_filters_active,
+            themes_actives: themes_actives,
             locations: original_state.location_filters,
-            locations_active: new_locations_actives
+            locations_active: location_actives
         })
     }
 }
--- react-src/reducers/catalog.js
+++ react-src/reducers/catalog.js
@@ -3,7 +3,6 @@ import {REQUEST_CATALOG, FILTER_CATALOG} from '../actions'
 const initialState = {
   catalog: [],
   originalCatalog: [],
-  // catalogFiltered: [],
   theme_filters: [],
   location_filters: [],
   theme_filters_active: [],
@@ -12,12 +11,9 @@ const initialState = {
 export function requestCatalog(state = initialState,action){
     switch (action.type) {
         case REQUEST_CATALOG:
-            console.log("mi action es REQUEST_CATALOG: ")
-            console.log(action)
             return Object.assign({}, state, {
                 catalog: action.payload,
                 originalCatalog: action.originalCatalog,
-                // catalogFiltered: action.catalogFiltered,
                 theme_filters: action.themes,
                 location_filters: action.locations,
                 theme_filters_active: [],
@@ -25,12 +21,9 @@ export function requestCatalog(state = initialState,action){
             })
             break;
         case FILTER_CATALOG:
-            console.log("mi action es FILTER_CATALOG: ")
-            console.log(action)
             return Object.assign({}, state, {
                 catalog: action.payload,
                 originalCatalog: action.originalCatalog,
-                // catalogFiltered: action.catalogFiltered,
                 theme_filters: action.themes,
                 theme_filters_active: action.themes_actives,
                 location_filters: action.locations,
