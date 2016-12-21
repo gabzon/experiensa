14551b418f33ccb5ea437a44df97e58d42867524
Víctor Valencia <valencia6x@gmail.com>
2016-12-21 15:32:06 -0400

    fixed background jump on scroll navigation

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -86,4 +86,15 @@ function smoothPageScroll(){
             }
         }
     });
+    if(navigator.userAgent.match(/Trident\/7\./)) {
+        jQuery('body').on("mousewheel", function () {
+            // remove default behavior
+            event.preventDefault();
+
+            //scroll without smoothing
+            var wheelDelta = event.wheelDelta;
+            var currentScrollPosition = window.pageYOffset;
+            window.scrollTo(0, currentScrollPosition - wheelDelta);
+        });
+    }
 }
