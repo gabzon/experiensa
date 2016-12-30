aaf0d06b59fe649b53ab3b1f6c79f8771d999735
Víctor Valencia <valencia6x@gmail.com>
2016-12-30 14:02:31 -0400

    deleted tailor code + unused other js

--- assets/scripts/client.js
+++
@@ -1,3 +0,0 @@
-  var socket = io('http://localhost:8080/experiensa');
-  socket.on('connect', function(){console.log("connectado a experiensa");});
-  socket.on('disconnect', function(){console.log("disconnecta de experiensa");});
