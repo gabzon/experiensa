5109dbe079bdc61961d914d93f63efde1b6af0f8
Víctor Valencia <valencia6x@gmail.com>
2017-01-30 15:14:15 -0400

    starting masonry catalog layout

--- react-src/catalog/components/CatalogLayout.js
+++ react-src/catalog/components/CatalogLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CatalogVoyageCards from './CatalogVoyageCards'
 import CatalogVoyageMinimalist from './CatalogVoyageMinimalist'
+import CatalogVoyageMasonry from './voyages/layouts/masonry/CatalogVoyageMasonry'
 
 export default class CatalogLayout extends React.Component {
     constructor(){
@@ -9,11 +10,11 @@ export default class CatalogLayout extends React.Component {
     renderVoyages(){
         if(this.props.voyages) {
             return this.props.voyages.map((voyage) => {
-                if(this.props.options.type =='cards'){
+                if (this.props.options.type == 'cards') {
                     return (
                         <CatalogVoyageCards voyage={voyage} key={voyage.index} options={this.props.options}/>
                     )
-                }else {
+                } else {
                     return (
                         <CatalogVoyageMinimalist voyage={voyage} key={voyage.index} options={this.props.options}/>
                     )
@@ -23,11 +24,30 @@ export default class CatalogLayout extends React.Component {
             return(<h1>No Voyages Available</h1>);
         }
     }
+    renderMasonryVoyages(){
+        if(this.props.voyages) {
+            return this.props.voyages.map((voyage) => {
+                return (
+                    <CatalogVoyageMasonry voyage={voyage} key={voyage.index} options={this.props.options}/>
+                )
+            })
+        }else{
+            return(<h1>No Voyages Available</h1>);
+        }
+    }
     render() {
-        return (
-            <div className="ui four stackable cards">
-                {this.renderVoyages()}
-            </div>
-        );
+        if(this.props.options.type == 'masonry') {
+            return (
+                <div className="ui three column doubling stackable masonry grid">
+                    {this.renderMasonryVoyages()}
+                </div>
+            );
+        }else{
+            return (
+                <div className="ui four stackable cards">>
+                    {this.renderVoyages()}
+                </div>
+            );
+        }
     }
 }
---
+++ react-src/catalog/components/voyages/layouts/masonry/CatalogVoyageMasonry.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Minimalist from '../VoyageImageMinimalist'
+import CatalogVoyageDetailsModalMinimalist from '../CatalogVoyageDetailsModalMinimalist'
+
+export default class CatalogVoyageMasonry extends React.Component {
+    constructor(){
+        super()
+    }
+    createTitleRow(value,show){
+        if(show && value){
+            return(
+                <div className="header">{value}</div>
+            )
+        }
+        return(<div></div>)
+    }
+    createDataRow(title,value,show){
+        if(show && value) {
+            return (
+                <div>
+                    <br/><strong>{title}: </strong>{value}
+                </div>
+            );
+        }
+        return(<div></div>);
+    }
+    render() {
+        // console.log(this.props.voyage);
+        let voyage = this.props.voyage;
+        let currency = "USD";
+        if(voyage.currency!=null)
+            currency = voyage.currency;
+        let price = "No Available";
+        if(voyage.price != '')
+            price = voyage.price;
+        if(price != 'No Available')
+            price = currency+' '+price;
+        return (
+            <div className="ui fluid card">
+                <Minimalist voyage={this.props.voyage}/>
+                <div className="content">
+                    {this.createTitleRow(voyage.title,this.props.options.title)}
+                    {this.createDataRow('Duration',voyage.duration,this.props.options.duration)}
+                    {this.createDataRow('Theme',voyage.theme,this.props.options.themes)}
+                    {this.createDataRow('Places',voyage.location,this.props.options.location)}
+                    {this.createDataRow('Country',voyage.country,this.props.options.country)}
+                </div>
+                <CatalogVoyageDetailsModalMinimalist voyage={this.props.voyage} price={price} options={this.props.options}/>
+            </div>
+        );
+    }
+}
