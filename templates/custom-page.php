a04abb72f228451086e4e96356c42168403caa93
Víctor Valencia <valencia6x@gmail.com>
2017-02-17 10:33:55 -0400

    test footer

--- templates/custom-page.php
+++ templates/custom-page.php
@@ -6,6 +6,9 @@
 <!-- Container Page -->
 <div class="ui container">
     <?php while (have_posts()) : the_post(); ?>
-        <?php the_content(); ?>
+        <?php
+            $content = get_the_content();
+            echo $content;
+        ?>
     <?php endwhile; ?>
 </div>
