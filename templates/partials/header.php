f115af764eb1133292cf2d5edd4733eccbd9b850
Víctor Valencia <valencia6x@gmail.com>
2017-02-17 11:11:26 -0400

    Header and Footer hidden by default

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,4 +1,5 @@
 <?php
-$header_style = Header::get_header_layout();
-$page_id = get_the_ID();
-Header::get_header($header_style);
+//$header_style = Header::get_header_layout();
+//$page_id = get_the_ID();
+//Header::get_header($header_style);
+echo dslc_hf_get_header();
